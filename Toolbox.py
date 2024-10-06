911d0512d3b8bb2715f187f20e5e6c053e6409ac
akhalii <nguyenkh022505@gmail.com>
2024-10-05 22:45:59 -0500

    Update Toolbox.py

--- Toolbox.py
+++ Toolbox.py
@@ -212,3 +212,40 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#_______________________________________________________________________________
+
+def user_input():
+    while True:
+        try:
+            days_collected = int(input("How many days have you been collecting bugs? "))
+            if days_collected < 0:
+                print("Please enter a postive number.")
+            else:
+                return days_collected
+
+def collect_bugs(day_number):
+    while True:
+        try:
+            bugs = int(input(f"Enter number of bugs collected on Day {day_number}: "))
+            if bugs < 0:
+                print("Please enter a non-negative.")
+            else:
+                return bugs
+        except ValueError:
+            print("Invalid input. Please enter numbers.")
+
+def calculate_total_bugs():
+    total = 0
+    for day in range(1,6):
+        bug_collected = collect_bugs(day)
+        total += bug_collected
+    return total
+
+def main():
+    print("Bug collecter program")
+    total = calculate_total_bugs()
+    print(f"Total bugs collected over five days: {total}")
+
+if __name__ == "__main__":
+    main()
