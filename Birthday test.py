8d160499cc259396fd9042f868bb240909bc148d
akhalii <nguyenkh022505@gmail.com>
2025-01-29 21:04:58 -0600

    Delete Birthday test.py

--- Birthday test.py
+++
@@ -1,9 +0,0 @@
-name = input("What is your name? ")
-age = int(input("How old are you? "))
-birthday = input("Is it your birthday? ")
-age2 = age + 1
-
-if (birthday) == "yes":
-    print(f"Happy {age2} birthday!")
-else:
-    print("I'll see you on your birthday!")
